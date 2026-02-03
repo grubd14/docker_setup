c0f624080147176307613c2e1f58f8612f2e6267
grubd14 <83056016+grubd14@users.noreply.github.com>
2026-02-03 20:32:12 +0545

    add: PHP app, update Docker config and gitignore
    Bump base image to php:8.5.
    Restructure docker-compose: replace web with php-web,
    Add .gitignore to ignore db_data.

---
+++ php_setup/src/index.php
@@ -0,0 +1,20 @@
+<?php
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+
+<body>
+echo "Hello Wordl!!"
+?>
+
+<?php
+//represents single line comment
+# this is also a single line comment
+/* this is used for multiple line comments */
+?>
+</body>
+</head>
+</html>
