ca1ed5e255dfe9f349fed0023b0e8b12b3a3bcab
grubd14 <83056016+grubd14@users.noreply.github.com>
2026-02-25 11:49:50 +0545

    simplified the form.php

--- php_setup/src/form.php
+++ php_setup/src/form.php
@@ -1,40 +1,29 @@
 <?php
-// Database connection
+// Database connection info
 $servername = "db";
 $username = "app_user";
 $password = "app_pass";
 $dbname = "app_db";
 
+// Connect to database
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get data from POST
-$first_name = isset($_POST["fname"]) ? $_POST["fname"] : "";
-$last_name = isset($_POST["lname"]) ? $_POST["lname"] : "";
-$email = isset($_POST["email"]) ? $_POST["email"] : "";
+// Get form data
+$first_name = $_POST["fname"];
+$last_name = $_POST["lname"];
+$email = $_POST["email"];
 
-// Only insert if required fields are present
-if ($first_name && $last_name && $email) {
-    $insertSql =
-        "INSERT INTO Users (firstname, lastname, email) VALUES (?, ?, ?)";
-
-    $stmt = mysqli_prepare($conn, $insertSql);
-    mysqli_stmt_bind_param($stmt, "sss", $first_name, $last_name, $email);
-
-    if (mysqli_stmt_execute($stmt)) {
-        echo "Welcome $first_name $last_name <br>";
-        echo "Email: $email <br>";
-        echo "<br>Data inserted successfully!";
-    } else {
-        echo "Error inserting data: " . mysqli_error($conn);
-    }
-
-    mysqli_stmt_close($stmt);
+// Insert data
+$sql = "INSERT INTO Users (firstname, lastname, email) VALUES ('$first_name', '$last_name', '$email')";
+if (mysqli_query($conn, $sql)) {
+    echo "User added: $first_name $last_name ($email)";
 } else {
-    echo "Please fill in all required fields (First Name, Last Name, Email).";
+    echo "Error: " . mysqli_error($conn);
 }
 
 mysqli_close($conn);
